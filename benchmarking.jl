016390e859970a30b69424d562b1474ae224fa55
higorsmonteiro <higormonteiros@gmail.com>
2020-11-19 22:56:16 -0300

    updt

--- benchmarking.jl
+++ benchmarking.jl
@@ -1,7 +1,7 @@
 #=
     Utility functions for proper benchmarking between FFP and MBC.
 =#
-push!(LOAD_PATH, "../"); using netx
+push!(LOAD_PATH, ""); using netx
 using BenchmarkTools
 
 function open_random_net(path::String, N::Int, k::Float64, ntype::Int, samp::Int)
@@ -26,7 +26,7 @@ function process_samples_ffp(N::Int, ntype::Int, nsamples::Int, path::String)
         for m in 1:nsamples
             print("$m ")
             g = open_random_net(path, N, k_mean, ntype, m)
-            b = @benchmarkable fast_fibration($g)
+            b = @benchmarkable netx.fast_fibration($g)
             tune!(b)
             res = minimum(run(b))
             g = netx.Graph(true) # check garbage collector
@@ -43,28 +43,35 @@ end
 """
 function do_measure(path::String, nsamples::Int, fout::String)
     ntypes = Int[1,2,4,8]
-    sizes = Int[256, 512, 1024, 2048, 4096, 8192, 16384]
+    sizes = Int[512]
 
-    time_dict = Dict{Int, Array{Float64}}()
-    space_dict = Dict{Int, Array{Float64}}()
-    for N in sizes
-        time_dict[N] = Float[ 0.0, 0.0, 0.0, 0.0 ]
-        space_dict[N] = Float[ 0.0, 0.0, 0.0, 0.0 ]
-        for (k, nt) in enumerate(ntypes)
+    time_dicts = [ Dict{Int, Array{Float64}}() for i in 1:length(ntypes) ]
+    space_dicts = [ Dict{Int, Array{Float64}}() for i in 1:length(ntypes) ]
+    for (k, nt) in enumerate(ntypes)
+        time_dict = time_dicts[k]
+        space_dict = space_dicts[k]
+        for N in sizes
+            time_dict[N] = Float64[ 0.0, 0.0, 0.0, 0.0 ]
+            space_dict[N] = Float64[ 0.0, 0.0, 0.0, 0.0 ]
             time_measures, space_measures = process_samples_ffp(N, nt, nsamples, path)
-            time_dict[N][k] = mean(time_measures)
-            space_dict[N][k] = mean(space_measures)
+            for n in 1:4
+                time_dict[N][n] = mean(time_measures[n])
+                space_dict[N][n] = mean(space_measures[n])
+            end
         end
     end
 
-    # -- Write to file --
-    open(fout, "w") do f
-        for N in sizes
-            write(f,"$N")
-            for (k, nt) in enumerate(ntypes)
-                write(f, "\t$(time_dict[N][k])\t$(space_dict[N][k])")
+    for (k, nt) in enumerate(ntypes)
+        time_dict = time_dicts[k]
+        space_dict = space_dicts[k]
+        open("teste_$(nt).txt", "w") do f
+            for N in sizes
+                write(f,"$N")
+                for (k, nt) in enumerate(ntypes)
+                    write(f, "\t$(time_dict[N][k])\t$(space_dict[N][k])")
+                end
+                write(f,"\n")
             end
-            write(f,"\n")
         end
     end
 end
