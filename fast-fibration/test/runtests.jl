f2f88f992a03e97f3307619411d95bb291703d39
higorsmonteiro <higormonteiros@gmail.com>
2021-05-05 22:52:48 -0300

    adding simple tests for CI verification

--- fast-fibration/test/runtests.jl
+++ fast-fibration/test/runtests.jl
@@ -1,6 +1,9 @@
-using fast-fibration
+using FastFibration
 using Test
 
-@testset "fast-fibration.jl" begin
-    # Write your tests here.
+@testset "FastFibration.jl" begin
+    # Testing vertex obj
+    @test FastFibration.Vertex(0).index == 0
+    @test length(FastFibration.Vertex(0).edges_source) == 0
+    @test length(FastFibration.Vertex(0).edges_target) == 0
 end
